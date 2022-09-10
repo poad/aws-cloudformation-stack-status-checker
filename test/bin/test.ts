2ab2b083731890abf8493eb4d7f4903a0b3173b7
Kenji Saito <ken-yo@mbr.nifty.com>
2022-09-11 00:34:30 +0900

    Fix cdk deploy

--- test/bin/test.ts
+++ test/bin/test.ts
@@ -3,12 +3,17 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { TestStack } from '../lib/test-stack';
 
+
 const app = new cdk.App();
 
-const timestamp = app.node.tryGetContext('timestamp') as string | undefined;
+const region = app.node.tryGetContext('region');
+const timestamp = app.node.tryGetContext('timestamp');
+
+const env = region ? { account: app.account, region } : { account: app.account, region: app.region };
 
 const appName = 'aws-cloudformation-stack-status-checker-test';
 new TestStack(app, `${appName}${timestamp ? `-${timestamp}` : ''}`, {
   appName,
-  timestamp
+  timestamp,
+  env,
 });
