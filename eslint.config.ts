db9dd57fcfde6ac0c46110268a0465037ac312aa
Kenji Saito <ken-yo@mbr.nifty.com>
2025-09-19 03:09:03 +0900

    Fix the same

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,5 +1,4 @@
-// @ts-check
-
+import { defineConfig } from 'eslint/config';
 import eslint from '@eslint/js';
 import stylistic from '@stylistic/eslint-plugin';
 
@@ -7,10 +6,7 @@ import github from 'eslint-plugin-github';
 
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
+export default defineConfig(
   {
     ignores: [
       '**/*.d.ts',
@@ -18,18 +14,24 @@ export default tseslint.config(
       'node_modules/**/*',
       'dist',
     ],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  {
     files: ['src/**/*.{mts,ts}', 'test/**/*.{js,ts}'],
-    extends: [
-      ...tseslint.configs.recommended,
-    ],
     plugins: {
       '@stylistic': stylistic,
-      '@stylistic/ts': stylistic,
       github,
+    },
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig-eslint.json',
+      },
     },
     rules: {
       '@stylistic/semi': ['error', 'always'],
-      '@stylistic/ts/indent': ['error', 2],
+      '@stylistic/indent': ['error', 2],
       '@stylistic/max-len': ['error', 80],
       '@stylistic/comma-dangle': ['error', 'always-multiline'],
       '@stylistic/quotes': ['error', 'single'],
